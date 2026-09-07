45a077f065e9ad7ffcffa44b1e44da4e0399fd90
RubberDuckHero <sam.morgan.sp1@gmail.com>
2026-09-07 10:13:45 +0200

    road length

--- hott/src/terrain/terrainTypes.js
+++ hott/src/terrain/terrainTypes.js
@@ -346,7 +346,7 @@ function generateRoad(){
         definition.maxWidth,
         2
     )
-    const height = CONFIG.tableSize
+    const height = CONFIG.tableSize * 2
     const x = randomBetween(
         width / 2,
         CONFIG.tableSize - width / 2
